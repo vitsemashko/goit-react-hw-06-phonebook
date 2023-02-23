90ff3a9c49b98d7df7dbda140f0f1e45579b7d47
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2023-02-23 16:12:12 +0200

    Updated
    1) removed local state for filter, filter get value from redux store;
    2) added filter to blacklist in persist Config;
    3) repaired form reseting after adding an existed user to the contacts list;

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addFilter } from "../../redux/features/contact/contactSlice";
-import css from "./Filter.module.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { addFilter } from '../../redux/features/contact/contactSlice';
+import css from './Filter.module.css';
 const Filter = () => {
-	const dispatch = useDispatch();
-	const [value, setValue] = useState("");
-	const onValueChange = (e) => {
-		setValue(e.target.value);
-		dispatch(addFilter(e.target.value));
-	};
-	return (
-		<div className={css.filterWrapper}>
-			<h2>Find contacts by name</h2>
-			<input type="text" value={value} onChange={onValueChange} />
-		</div>
-	);
+  const dispatch = useDispatch();
+  const filter = useSelector(state => {
+    return state.contact.filter;
+  });
+  const onValueChange = e => {
+    dispatch(addFilter(e.target.value));
+  };
+  return (
+    <div className={css.filterWrapper}>
+      <h2>Find contacts by name</h2>
+      <input type="text" value={filter} onChange={onValueChange} />
+    </div>
+  );
 };
 
 export default Filter;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
 import {
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import contactReducer from "./features/contact/contactSlice";
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactReducer from './features/contact/contactSlice';
 
 const persistConfig = {
-	key: "root",
-	storage,
+  key: 'root',
+  storage,
+  blacklist: ['filter'],
 };
 
 const persistedContactReducer = persistReducer(persistConfig, contactReducer);
 
 export const store = configureStore({
-	reducer: {
-		contact: persistedContactReducer,
-	},
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}),
+  reducer: {
+    contact: persistedContactReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export const persistor = persistStore(store);
